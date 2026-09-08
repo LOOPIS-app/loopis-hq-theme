33e221b4c498150fe6bd0d81cdb656de39754d00
joxyzan <johan.hagvil@gmail.com>
2026-09-08 10:51:02 +0200

    member settings

--- pages/user/member-data.php
+++ pages/user/member-data.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<h3>📝 Medlemsregister</h3>
+<h3>🖊 Medlemsregister</h3>
 <hr>
 <p class="small">💡 Ange dina aktuella uppgifter.</p>
 
@@ -21,3 +21,4 @@ include LOOPIS_THEME_HQ_DIR . '/templates/forms/member-form.php'; ?>
 
 <p>Läs hur föreningen hanterar personuppgifter: <span class="big-link"><a href="<?php echo esc_url(home_url('/privacy/')); ?>">🗄 Integritet</a></span></p>
 <p>Läs föreningens stadgar: <span class="big-link"><a href="<?php echo esc_url(home_url('/faq/loopis-stadgar')); ?>">📜 Stadgar</a></span></p>
+<p>För byte av användarnamn eller epost-adress: <span class="big-link"><a href="mailto:info@loopis.app">✉ info@loopis.app</a></span></p>
--- pages/user/tabs/settings.php
+++ pages/user/tabs/settings.php
@@ -18,15 +18,13 @@ $user = wp_get_current_user();
 <hr>
 <p class="small">💡 Inställningar för ditt medlemskap.</p>
 
-<div class="wrapped link" onclick="location.href='<?php echo esc_url(home_url('/user/?view=member-data')); ?>'">
-<h5>📝 Medlemsregister</h5>
-<hr>
+<div class="wrapped">
 <p>👤 Användarnamn: <b><?php echo $user->user_login ?></b></p>
 <p>✉ E-post: <b><?php echo antispambot($user->user_email); ?></b></p>
 <p>📱 Mobilnummer: <b><span class="unclickable"><?php echo antispambot($user->wpum_phone); ?></span></b></p>
-<p>🗺 Bostadsort: <b><?php include LOOPIS_THEME_DIR . '/includes/output/user-data/user-city.php'; ?></b></p>
-<p>📍 Område: <b><?php include LOOPIS_THEME_DIR . '/includes/output/user-data/user-primary-blog.php'; ?></b></p>
 </div>
 
-<p><span class="big-link"><a href="<?php echo esc_url(get_author_posts_url($user_id)); ?>">👥 Din profil</a></span> som den visas för andra</p>
-<p><span class="big-link"><a href="<?php echo esc_url(wp_logout_url(home_url())); ?>">🚪 Logga ut</a></span> från LOOPIS.app</p>
+<p><span class="big-link"><a href="<?php echo esc_url(home_url('/user/?view=member-data')); ?>">🖊 Medlemsregister</a></span></p>
+<p><span class="big-link"><a href="<?php echo esc_url(get_author_posts_url($user_id)); ?>">👥 Se din profil</a></span></p>
+<p><span class="big-link"><a href="<?php echo esc_url(home_url('/wp-login.php?action=lostpassword')); ?>">🔑 Byt lösenord</a></span></p>
+<p><span class="big-link"><a href="<?php echo esc_url(wp_logout_url(home_url())); ?>">🚪 Logga ut</a></span></p>
